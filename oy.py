4eed1fa51d8a5842073867c741fd610f3f67709b
Dattu219 <m.dattasai067@gmail.com>
2022-03-24 14:17:10 +0530

    fourth commit

--- oy.py
+++ oy.py
@@ -2,9 +2,9 @@ import subprocess as sb
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/Dattu219/ContractWard/main/OpenSourceContractInfo_without_code.csv')
-df = df.loc[: , 'address']
+# df = df.loc[: , 'address']
 print(df.dtypes)
 
-# for i in range(5):
-#     cmd = 'python oyente.py -ru https://raw.githubusercontent.com/Dattu219/ContractWard/main/sol_149363/' + df['address'][i]
-#     sb.check_call(cmd)
+for i in range(5):
+    cmd = 'python oyente.py -ru https://raw.githubusercontent.com/Dattu219/ContractWard/main/sol_149363/' + df['address'][i]
+    sb.check_call(cmd)
